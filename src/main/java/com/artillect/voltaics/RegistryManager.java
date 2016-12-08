f4fc3375029e687a2540855985de37bac6c87f56
HenryDane <aglareb7@gmail.com>
2016-12-07 20:16:38 -0800

    began developing turret entity

--- src/main/java/com/artillect/voltaics/RegistryManager.java
+++ src/main/java/com/artillect/voltaics/RegistryManager.java
@@ -6,6 +6,8 @@ import java.util.Random;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.init.Biomes;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -14,13 +16,18 @@ import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkGenerator;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenMinable;
+import net.minecraft.world.storage.loot.LootTableList;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.IWorldGenerator;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import com.artillect.voltaics.tileentity.*;
 import com.artillect.voltaics.block.*;
+import com.artillect.voltaics.entity.EntityTurret;
+import com.artillect.voltaics.entity.RenderEntityTurret;
 import com.artillect.voltaics.item.*;
 
 public class RegistryManager {
--- src/main/java/com/artillect/voltaics/Voltaics.java
+++ src/main/java/com/artillect/voltaics/Voltaics.java
@@ -1,13 +1,18 @@
 package com.artillect.voltaics;
 
 import com.artillect.voltaics.client.VoltaicsTab;
+import com.artillect.voltaics.entity.EntityTurret;
+import com.artillect.voltaics.entity.RenderEntityTurret;
 import com.artillect.voltaics.proxy.CommonProxy;
 
+import net.minecraft.world.storage.loot.LootTableList;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
 
 @Mod(modid = Voltaics.modId, name = Voltaics.name, version = Voltaics.version, acceptedMinecraftVersions = "[1.10.2]")
 public class Voltaics {
@@ -28,6 +33,16 @@ public class Voltaics {
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event) {
 		RegistryManager.initR(); //initalize recepies
+		int id = 1;
+        EntityRegistry.registerModEntity(EntityTurret.class, "Turret", id++, Voltaics.instance, 64, 3, true, 0x996600, 0x00ff00);
+
+        // We want our mob to spawn in Plains and ice plains biomes. If you don't add this then it will not spawn automatically
+        // but you can of course still make it spawn manually
+        //EntityRegistry.addSpawn(EntityTurret.class, 100, 3, 5, EnumCreatureType.MONSTER, Biomes.PLAINS, Biomes.ICE_PLAINS);
+
+        // This is the loot table for our mob
+        LootTableList.register(EntityTurret.LOOT);
+        RenderingRegistry.registerEntityRenderingHandler(EntityTurret.class, RenderEntityTurret.FACTORY);
 	}
 
 	@Mod.EventHandler
