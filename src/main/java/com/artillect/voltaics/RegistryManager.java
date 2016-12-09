6aa1d36342aa83c3d9bcd2070e5cad4ad08db2fd
stopmotionmanager <oreo4455@gmail.com>
2016-12-08 19:49:06 -0800

    Re-added registrymanager

--- src/main/java/com/artillect/voltaics/RegistryManager.java
+++ src/main/java/com/artillect/voltaics/RegistryManager.java
@@ -1,6 +1,7 @@
 package com.artillect.voltaics;
 
 import java.util.ArrayList;
+<<<<<<< HEAD
 import java.util.Random;
 
 import net.minecraft.block.Block;
@@ -21,6 +22,24 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.oredict.OreDictionary;
 
 <<<<<<< HEAD
+=======
+
+import com.artillect.voltaics.block.IModeledBlock;
+import com.artillect.voltaics.item.IModeledItem;
+import com.artillect.voltaics.item.ItemBase;
+import com.artillect.voltaics.item.ItemHammer;
+import com.artillect.voltaics.item.ItemVoltmeter;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.Minecraft;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+>>>>>>> 557d1ce... Re-added registrymanager
 import com.artillect.voltaics.tileentity.TileEntityInductor;
 //import com.artillect.voltaics.tileentity.TileEntityInductor;
 import com.artillect.voltaics.tileentity.TileEntityLowVoltageConduit;
@@ -28,16 +47,20 @@ import com.artillect.voltaics.tileentity.TileEntityVoltaicPile;
 import com.artillect.voltaics.block.BlockInductor;
 import com.artillect.voltaics.block.BlockLowVoltageConduit;
 import com.artillect.voltaics.block.BlockVoltaicPile;
+<<<<<<< HEAD
 =======
 import com.artillect.voltaics.tileentity.*;
 import com.artillect.voltaics.block.*;
 import com.artillect.voltaics.item.*;
 >>>>>>> 24f19b6... fixed copper ore
+=======
+>>>>>>> 557d1ce... Re-added registrymanager
 
 public class RegistryManager {
 	public static ArrayList<Block> blocks = new ArrayList<Block>();
 	public static ArrayList<Item> items = new ArrayList<Item>();
 
+<<<<<<< HEAD
 	public static Item voltmeter, ingotCopper, hammer, ingotdcopper;
 	public static Block voltaicPile, lowVoltageConduit, inductor, copperOre,
 		duranizedcopperore;
@@ -54,12 +77,32 @@ public class RegistryManager {
 		//add blocks
 		blocks.add(duranizedcopperore = (new BlockDuranizedCopperOre("dCopperOre")).setCreativeTab(CreativeTabs.BUILDING_BLOCKS));
 		blocks.add(copperOre = (new BlockCopperOre("copperOre")).setCreativeTab(CreativeTabs.BUILDING_BLOCKS));
+=======
+	public static Item voltmeter, ingotCopper, hammer;
+	public static Block voltaicPile, lowVoltageConduit, inductor;
+	
+	public static void registerAll(){
+		//blocks.add(blockCaminiteLargeBrick = (new BlockBase(Material.ROCK,"blockCaminiteLargeBrick",true)).setHarvestProperties("pickaxe", 0).setHardness(1.6f));
+		
+		/*FluidRegistry.registerFluid(fluidMoltenUmberSteel = new FluidMoltenUmberSteel());
+		blocks.add(blockMoltenUmberSteel = (new BlockMoltenUmberSteel("moltenUmberSteel",false)));
+		FluidRegistry.addBucketForFluid(fluidMoltenUmberSteel);
+		
+		FluidRegistry.registerFluid(fluidMoltenAstralite = new FluidMoltenAstralite());
+		blocks.add(blockMoltenAstralite = (new BlockMoltenAstralite("moltenAstralite",false)));
+		FluidRegistry.addBucketForFluid(fluidMoltenAstralite);*/
+		items.add(voltmeter = new ItemVoltmeter());
+		items.add(ingotCopper = new ItemBase("ingotCopper",true));
+		items.add(hammer = new ItemHammer());
+		
+>>>>>>> 557d1ce... Re-added registrymanager
 		blocks.add(voltaicPile = (new BlockVoltaicPile(Material.ROCK,"voltaicPile",true)).setIsFullCube(false).setIsOpaqueCube(false).setHarvestProperties("pickaxe", 0).setHardness(1.4f));
 		blocks.add(lowVoltageConduit = (new BlockLowVoltageConduit(Material.ROCK,"lowVoltageConduit",true)).setIsFullCube(false).setIsOpaqueCube(false).setHarvestProperties("pickaxe", 0).setHardness(1.0f));
 		blocks.add(inductor = (new BlockInductor(Material.ROCK,"inductor",true)).setIsFullCube(false).setIsOpaqueCube(false).setHarvestProperties("pickaxe", 0).setHardness(1.4f));
 		GameRegistry.registerTileEntity(TileEntityVoltaicPile.class, Voltaics.modId+":tileEntityVoltaicPile");
 		GameRegistry.registerTileEntity(TileEntityLowVoltageConduit.class, Voltaics.modId+":tileEntityLowVoltageConduit");
 		GameRegistry.registerTileEntity(TileEntityInductor.class, Voltaics.modId+":tileEntityInductor");
+<<<<<<< HEAD
 		
 		//register ores
 		OreDictionary.registerOre("oreCopper", copperOre);
@@ -70,12 +113,15 @@ public class RegistryManager {
 		RegistryManager rm = new RegistryManager();
 		wgen = rm.new VoltaicsWorldGen();
 		GameRegistry.registerWorldGenerator(wgen, 0);
+=======
+>>>>>>> 557d1ce... Re-added registrymanager
 	}
 	
 	public static void registerFluids(){
 
 	}
 	
+<<<<<<< HEAD
 	public static void initR(){
 		GameRegistry.addSmelting(copperOre, new ItemStack(ingotCopper), 0f);
 		GameRegistry.addSmelting(duranizedcopperore, new ItemStack(ingotdcopper), 2f);
@@ -83,6 +129,11 @@ public class RegistryManager {
 	
 	@SideOnly(Side.CLIENT)
     public static void registerRendering(){
+=======
+	@SideOnly(Side.CLIENT)
+    public static void registerRendering(){
+
+>>>>>>> 557d1ce... Re-added registrymanager
 		for (int i = 0; i < blocks.size(); i ++){
 			if (blocks.get(i) instanceof IModeledBlock){
 				((IModeledBlock)blocks.get(i)).initModel();
@@ -94,6 +145,7 @@ public class RegistryManager {
 			}
 		}
 	}
+<<<<<<< HEAD
 	
 	class VoltaicsWorldGen implements IWorldGenerator {
 		@Override
@@ -125,3 +177,6 @@ public class RegistryManager {
 		}
 	}
 }
+=======
+}
+>>>>>>> 557d1ce... Re-added registrymanager
