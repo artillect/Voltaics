2b3cc1cd812309394a44e2682f8f6d29bb630030
Riley <oreo4455@gmail.com>
2017-11-09 15:06:50 -0800

    Fixed issues introduced by rebase

--- src/main/java/com/artillect/voltaics/RegistryManager.java
+++ src/main/java/com/artillect/voltaics/RegistryManager.java
@@ -1,46 +1,23 @@
-<<<<<<< HEAD
 package com.artillect.voltaics;
 
 import java.util.ArrayList;
-<<<<<<< HEAD
-import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import net.minecraft.world.chunk.IChunkGenerator;
-import net.minecraft.world.chunk.IChunkProvider;
-import net.minecraft.world.gen.feature.WorldGenMinable;
-import net.minecraftforge.fml.common.IWorldGenerator;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.oredict.OreDictionary;
 
-<<<<<<< HEAD
-=======
-
 import com.artillect.voltaics.block.IModeledBlock;
 import com.artillect.voltaics.item.IModeledItem;
 import com.artillect.voltaics.item.ItemBase;
 import com.artillect.voltaics.item.ItemHammer;
 import com.artillect.voltaics.item.ItemVoltmeter;
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.client.Minecraft;
-import net.minecraft.item.Item;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
-import net.minecraftforge.fml.client.registry.RenderingRegistry;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
->>>>>>> 557d1ce... Re-added registrymanager
+
 import com.artillect.voltaics.tileentity.TileEntityInductor;
 //import com.artillect.voltaics.tileentity.TileEntityInductor;
 import com.artillect.voltaics.tileentity.TileEntityLowVoltageConduit;
@@ -48,205 +25,41 @@ import com.artillect.voltaics.tileentity.TileEntityVoltaicPile;
 import com.artillect.voltaics.block.BlockInductor;
 import com.artillect.voltaics.block.BlockLowVoltageConduit;
 import com.artillect.voltaics.block.BlockVoltaicPile;
-<<<<<<< HEAD
-=======
-import com.artillect.voltaics.tileentity.*;
-import com.artillect.voltaics.block.*;
-import com.artillect.voltaics.item.*;
->>>>>>> 24f19b6... fixed copper ore
-=======
->>>>>>> 557d1ce... Re-added registrymanager
+
 
 public class RegistryManager {
 	public static ArrayList<Block> blocks = new ArrayList<Block>();
 	public static ArrayList<Item> items = new ArrayList<Item>();
-
-<<<<<<< HEAD
-	public static Item voltmeter, ingotCopper, hammer, ingotdcopper;
-	public static Block voltaicPile, lowVoltageConduit, inductor, copperOre,
-		duranizedcopperore;
-	
-	public static void registerAll(){
-		VoltaicsWorldGen wgen;	//world generator
-		
-		//add items
-		items.add(voltmeter = new ItemVoltmeter());
-		items.add(ingotCopper = new ItemBase("ingotCopper",true));
-		items.add(ingotdcopper = new ItemBase("ingotDuranizedCopper",true));
-		items.add(hammer = new ItemHammer());
-		
-		//add blocks
-		blocks.add(duranizedcopperore = (new BlockDuranizedCopperOre("dCopperOre")).setCreativeTab(CreativeTabs.BUILDING_BLOCKS));
-		blocks.add(copperOre = (new BlockCopperOre("copperOre")).setCreativeTab(CreativeTabs.BUILDING_BLOCKS));
-=======
 	public static Item voltmeter, ingotCopper, hammer;
-	public static Block voltaicPile, lowVoltageConduit, inductor;
+	public static Block voltaicPile, lowVoltageConduit, inductor, copperOre;
 	
 	public static void registerAll(){
-		//blocks.add(blockCaminiteLargeBrick = (new BlockBase(Material.ROCK,"blockCaminiteLargeBrick",true)).setHarvestProperties("pickaxe", 0).setHardness(1.6f));
-		
-		/*FluidRegistry.registerFluid(fluidMoltenUmberSteel = new FluidMoltenUmberSteel());
-		blocks.add(blockMoltenUmberSteel = (new BlockMoltenUmberSteel("moltenUmberSteel",false)));
-		FluidRegistry.addBucketForFluid(fluidMoltenUmberSteel);
-		
-		FluidRegistry.registerFluid(fluidMoltenAstralite = new FluidMoltenAstralite());
-		blocks.add(blockMoltenAstralite = (new BlockMoltenAstralite("moltenAstralite",false)));
-		FluidRegistry.addBucketForFluid(fluidMoltenAstralite);*/
+
 		items.add(voltmeter = new ItemVoltmeter());
 		items.add(ingotCopper = new ItemBase("ingotCopper",true));
 		items.add(hammer = new ItemHammer());
-		
->>>>>>> 557d1ce... Re-added registrymanager
 		blocks.add(voltaicPile = (new BlockVoltaicPile(Material.ROCK,"voltaicPile",true)).setIsFullCube(false).setIsOpaqueCube(false).setHarvestProperties("pickaxe", 0).setHardness(1.4f));
 		blocks.add(lowVoltageConduit = (new BlockLowVoltageConduit(Material.ROCK,"lowVoltageConduit",true)).setIsFullCube(false).setIsOpaqueCube(false).setHarvestProperties("pickaxe", 0).setHardness(1.0f));
 		blocks.add(inductor = (new BlockInductor(Material.ROCK,"inductor",true)).setIsFullCube(false).setIsOpaqueCube(false).setHarvestProperties("pickaxe", 0).setHardness(1.4f));
 		GameRegistry.registerTileEntity(TileEntityVoltaicPile.class, Voltaics.modId+":tileEntityVoltaicPile");
 		GameRegistry.registerTileEntity(TileEntityLowVoltageConduit.class, Voltaics.modId+":tileEntityLowVoltageConduit");
 		GameRegistry.registerTileEntity(TileEntityInductor.class, Voltaics.modId+":tileEntityInductor");
-<<<<<<< HEAD
 		
 		//register ores
 		OreDictionary.registerOre("oreCopper", copperOre);
 		OreDictionary.registerOre("ingotCopper", ingotCopper);
-		OreDictionary.registerOre("oreDuranium", duranizedcopperore);
-		
-		//register worldgen
-		RegistryManager rm = new RegistryManager();
-		wgen = rm.new VoltaicsWorldGen();
-		GameRegistry.registerWorldGenerator(wgen, 0);
-=======
->>>>>>> 557d1ce... Re-added registrymanager
 	}
 	
 	public static void registerFluids(){
 
 	}
 	
-<<<<<<< HEAD
 	public static void initR(){
 		GameRegistry.addSmelting(copperOre, new ItemStack(ingotCopper), 0f);
-		GameRegistry.addSmelting(duranizedcopperore, new ItemStack(ingotdcopper), 2f);
 	}
 	
 	@SideOnly(Side.CLIENT)
     public static void registerRendering(){
-=======
-	@SideOnly(Side.CLIENT)
-    public static void registerRendering(){
-
->>>>>>> 557d1ce... Re-added registrymanager
-		for (int i = 0; i < blocks.size(); i ++){
-			if (blocks.get(i) instanceof IModeledBlock){
-				((IModeledBlock)blocks.get(i)).initModel();
-			}
-		}
-		for (int i = 0; i < items.size(); i ++){
-			if (items.get(i) instanceof IModeledItem){
-				((IModeledItem)items.get(i)).initModel();
-			}
-		}
-	}
-<<<<<<< HEAD
-	
-	class VoltaicsWorldGen implements IWorldGenerator {
-		@Override
-		public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator,
-				IChunkProvider chunkProvider) {
-			switch (world.provider.getDimension()) {
-			case 0: 
-				generateOverworld(world, random, chunkX, chunkZ);
-				break;
-			}
-			
-		}
-
-		public void generateOverworld(World world, Random rand, int x, int z) {
-			generateOre(copperOre, world, rand, x, z, 2, 6, 4, 5, 100, Blocks.STONE);
-			generateOre(duranizedcopperore, world, rand, x, z, 2, 6, 4, 5, 10, Blocks.STONE);
-		}
-		
-		public void generateOre(Block block, World world, Random random, int chunkX, int chunkZ, int minVienSize, int maxVienSize, int chance, int minY, int maxY, Block generateIn) {
-			int vienSize = minVienSize + random.nextInt(maxVienSize - minVienSize);
-			int heightRange = maxY - minY;
-			WorldGenMinable gen = new WorldGenMinable(block.getDefaultState(), vienSize);
-			for(int i = 0; i < chance; i++) {
-				int xRand = chunkX * 16 + random.nextInt(16);
-				int yRand = random.nextInt(heightRange) + minY;
-				int zRand = chunkZ * 16 + random.nextInt(16);
-				gen.generate(world, random, new BlockPos(xRand, yRand, zRand));
-			}
-		}
-	}
-}
-=======
-}
->>>>>>> 557d1ce... Re-added registrymanager
-=======
-package com.artillect.voltaics;
-
-import java.util.ArrayList;
-
-import com.artillect.voltaics.block.IModeledBlock;
-import com.artillect.voltaics.item.IModeledItem;
-import com.artillect.voltaics.item.ItemBase;
-import com.artillect.voltaics.item.ItemHammer;
-import com.artillect.voltaics.item.ItemThermometer;
-import com.artillect.voltaics.item.ItemVoltmeter;
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.client.Minecraft;
-import net.minecraft.item.Item;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
-import net.minecraftforge.fml.client.registry.RenderingRegistry;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-
-import com.artillect.voltaics.tileentity.TileEntityInductor;
-//import com.artillect.voltaics.tileentity.TileEntityInductor;
-import com.artillect.voltaics.tileentity.TileEntityLowVoltageConduit;
-import com.artillect.voltaics.tileentity.TileEntityVoltaicPile;
-import com.artillect.voltaics.block.BlockInductor;
-import com.artillect.voltaics.block.BlockLowVoltageConduit;
-import com.artillect.voltaics.block.BlockVoltaicPile;
-
-public class RegistryManager {
-	public static ArrayList<Block> blocks = new ArrayList<Block>();
-	public static ArrayList<Item> items = new ArrayList<Item>();
-
-	public static Item voltmeter, ingotCopper, hammer, thermometer;
-	public static Block voltaicPile, lowVoltageConduit, inductor;
-	
-	public static void registerAll(){
-		//blocks.add(blockCaminiteLargeBrick = (new BlockBase(Material.ROCK,"blockCaminiteLargeBrick",true)).setHarvestProperties("pickaxe", 0).setHardness(1.6f));
-		
-		/*FluidRegistry.registerFluid(fluidMoltenUmberSteel = new FluidMoltenUmberSteel());
-		blocks.add(blockMoltenUmberSteel = (new BlockMoltenUmberSteel("moltenUmberSteel",false)));
-		FluidRegistry.addBucketForFluid(fluidMoltenUmberSteel);
-		
-		FluidRegistry.registerFluid(fluidMoltenAstralite = new FluidMoltenAstralite());
-		blocks.add(blockMoltenAstralite = (new BlockMoltenAstralite("moltenAstralite",false)));
-		FluidRegistry.addBucketForFluid(fluidMoltenAstralite);*/
-		items.add(voltmeter = new ItemVoltmeter());
-		items.add(ingotCopper = new ItemBase("ingotCopper",true));
-		items.add(hammer = new ItemHammer());
-		items.add(thermometer = new ItemThermometer());
-		
-		blocks.add(voltaicPile = (new BlockVoltaicPile(Material.ROCK,"voltaicPile",true)).setIsFullCube(false).setIsOpaqueCube(false).setHarvestProperties("pickaxe", 0).setHardness(1.4f));
-		blocks.add(lowVoltageConduit = (new BlockLowVoltageConduit(Material.ROCK,"lowVoltageConduit",true)).setIsFullCube(false).setIsOpaqueCube(false).setHarvestProperties("pickaxe", 0).setHardness(1.0f));
-		blocks.add(inductor = (new BlockInductor(Material.ROCK,"inductor",true)).setIsFullCube(false).setIsOpaqueCube(false).setHarvestProperties("pickaxe", 0).setHardness(1.4f));
-		GameRegistry.registerTileEntity(TileEntityVoltaicPile.class, Voltaics.modId+":tileEntityVoltaicPile");
-		GameRegistry.registerTileEntity(TileEntityLowVoltageConduit.class, Voltaics.modId+":tileEntityLowVoltageConduit");
-		GameRegistry.registerTileEntity(TileEntityInductor.class, Voltaics.modId+":tileEntityInductor");
-	}
-	
-	public static void registerFluids(){
-
-	}
-	
-	@SideOnly(Side.CLIENT)
-    public static void registerRendering(){
-
 		for (int i = 0; i < blocks.size(); i ++){
 			if (blocks.get(i) instanceof IModeledBlock){
 				((IModeledBlock)blocks.get(i)).initModel();
@@ -259,4 +72,3 @@ public class RegistryManager {
 		}
 	}
 }
->>>>>>> 9022cc8... Resyncing changes
--- src/main/java/com/artillect/voltaics/network/message/MessageTEUpdate.java
+++ src/main/java/com/artillect/voltaics/network/message/MessageTEUpdate.java
@@ -1,65 +1,3 @@
-<<<<<<< HEAD
-package com.artillect.voltaics.network.message;
-
-import io.netty.buffer.ByteBuf;
-import net.minecraft.client.Minecraft;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.math.BlockPos;
-import net.minecraftforge.fml.common.FMLCommonHandler;
-import net.minecraftforge.fml.common.network.ByteBufUtils;
-import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
-import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
-import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-
-public class MessageTEUpdate implements IMessage {
-	public NBTTagCompound tag = new NBTTagCompound();
-	
-	public MessageTEUpdate(){
-		//
-	}
-	
-	public MessageTEUpdate(TileEntity tile){
-		this.tag = tile.getUpdateTag();
-	}
-	
-	@Override
-	public void fromBytes(ByteBuf buf) {
-		tag = ByteBufUtils.readTag(buf);
-	}
-
-	@Override
-	public void toBytes(ByteBuf buf) {
-		ByteBufUtils.writeTag(buf, tag);
-	}
-
-    public static class MessageHolder implements IMessageHandler<MessageTEUpdate,IMessage>
-    {
-    	@SideOnly(Side.CLIENT)
-        @Override
-        public IMessage onMessage(final MessageTEUpdate message, final MessageContext ctx) {
-<<<<<<< HEAD
-    		Minecraft.getMinecraft().addScheduledTask(()-> {
-	    		if ((Minecraft.getMinecraft().player.getEntityWorld()).getTileEntity(new BlockPos(message.tag.getInteger("x"),message.tag.getInteger("y"),message.tag.getInteger("z"))) != null){
-	    			Minecraft.getMinecraft().player.getEntityWorld().getTileEntity(new BlockPos(message.tag.getInteger("x"),message.tag.getInteger("y"),message.tag.getInteger("z"))).readFromNBT(message.tag);
-	    			Minecraft.getMinecraft().player.getEntityWorld().getTileEntity(new BlockPos(message.tag.getInteger("x"),message.tag.getInteger("y"),message.tag.getInteger("z"))).markDirty();
-	    		}
-	    	});
-=======
-//    		Minecraft.getMinecraft().addScheduledTask(()-> {
-//	    		if ((Minecraft.getMinecraft().thePlayer.getEntityWorld()).getTileEntity(new BlockPos(message.tag.getInteger("x"),message.tag.getInteger("y"),message.tag.getInteger("z"))) != null){
-//	    			Minecraft.getMinecraft().thePlayer.getEntityWorld().getTileEntity(new BlockPos(message.tag.getInteger("x"),message.tag.getInteger("y"),message.tag.getInteger("z"))).readFromNBT(message.tag);
-//	    			Minecraft.getMinecraft().thePlayer.getEntityWorld().getTileEntity(new BlockPos(message.tag.getInteger("x"),message.tag.getInteger("y"),message.tag.getInteger("z"))).markDirty();
-//	    		}
-//	    	});
->>>>>>> 24f19b6... fixed copper ore
-    		return null;
-	    }
-    }
-}
-=======
 package com.artillect.voltaics.network.message;
 
 import io.netty.buffer.ByteBuf;
@@ -111,4 +49,3 @@ public class MessageTEUpdate implements IMessage {
 	    }
     }
 }
->>>>>>> 9022cc8... Resyncing changes
